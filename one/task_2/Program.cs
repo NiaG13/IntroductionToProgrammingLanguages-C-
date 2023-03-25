db75647404fe2a9011f2bf9db63158c72033ccdb
NiaG <13niag17@gmail.com>
2023-03-26 00:16:50 +0300

    Completed task_2

---
+++ one/task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// программа принимает два числа и выводит, какое большее, а какое меньшее.
+
+double a = 0;
+double b = 0;
+
+Console.Write("Enter numbers 'a' & 'b': ");
+
+if (double.TryParse(Console.ReadLine(), out a) && double.TryParse(Console.ReadLine(), out b))
+{
+    if (a == b)
+    {
+    Console.WriteLine("These numbers are equal");
+    }
+    else if ( a < b )
+    {
+    Console.WriteLine("Min = " + a);
+    Console.WriteLine("Max = " + b);
+    }
+    else
+    {
+    Console.WriteLine("Min = " + b);
+    Console.WriteLine("Max = " + a);
+    }
+}
+else
+{
+    Console.WriteLine("Not a number");
+}
