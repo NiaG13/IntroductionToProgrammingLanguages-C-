9fa0a01b417f5e529597a846902240d8a1cf30f8
NiaG <13niag17@gmail.com>
2023-03-26 02:03:57 +0300

    completed task_4

---
+++ one/task_4/Program.cs
@@ -0,0 +1,38 @@
+﻿// программа принимает три числа и выдаёт максимальное из них
+
+double a = 0;
+double b = 0;
+double c = 0;
+double max = a;
+
+Console.WriteLine("Enter three humbers:");
+
+if(double.TryParse(Console.ReadLine(), out a) && double.TryParse(Console.ReadLine(), out b) && double.TryParse(Console.ReadLine(), out c))
+{
+    if( b > max )
+    {
+        max = b;
+        if( max > c )
+        {
+            Console.WriteLine("max= " + max);
+        }
+        else
+        {
+            max = c;
+            Console.WriteLine("max= " + max);
+        }
+    }
+    else if( c > max )
+    {
+        max = c;
+        Console.WriteLine("max= " + max);
+    }
+    else
+    {
+        Console.WriteLine("max= " + max);
+    }
+}
+else
+{
+    Console.WriteLine("Not a number");
+}
