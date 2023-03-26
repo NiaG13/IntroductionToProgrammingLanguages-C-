83c5a102aadba1cdd9065a0962d2df6ad1f01bae
NiaG <13niag17@gmail.com>
2023-03-26 17:53:52 +0300

    completed task_8

---
+++ one/task_8/Program.cs
@@ -0,0 +1,33 @@
+﻿// программа принимает на вход число (N), а на выходе показывает все чётные числа от 1 до N
+
+int N = 0;
+
+Console.Write("Enter a number more than one: ");
+
+if(int.TryParse(Console.ReadLine(), out N) && N != 0)
+{
+    if( N > 0 )
+    {
+        for(int i = 1; i < N; i ++)
+        {
+            if( i % 2 == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+    else
+    {
+        for(int i = -1; i > N; i --)
+        {
+            if( i % 2 == 0 )
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Not a number");
+}
