456d4f5065345d9ba2f7864ec70d0726d88f7f14
NiaG <13niag17@gmail.com>
2023-03-26 17:13:29 +0300

    completed task_6

---
+++ one/task_6/Program.cs
@@ -0,0 +1,21 @@
+﻿//программа принимает на вход число и выдаёт, является ли оно чётным
+
+int a = 0;
+
+Console.Write("Enter an integer: ");
+
+if(int.TryParse(Console.ReadLine(), out a) && a != 0)
+{
+    if( a % 2 == 0 )
+    {
+        Console.WriteLine("This number is even");
+    }
+    else
+    {
+        Console.WriteLine("This number isn't even");
+    }
+}
+else
+{
+    Console.WriteLine("Not a number");
+}
