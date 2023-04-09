e74f42725d97b65f0501391d27b77aed14a8ca3e
NiaG <13niag17@gmail.com>
2023-04-09 16:18:46 +0300

    Completed HomeWork5

---
+++ hw_5/task_34/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Main();
+
+void Main()
+{
+    bool work = true;
+    while(work)
+    {
+        System.Console.WriteLine("Hello! Run the program? y/n");
+        string a = Console.ReadLine();
+        switch(a)
+        {
+            case "y": Task(); break;
+            case "n": work = false; break;
+            default: break;
+        }
+    }
+    System.Console.WriteLine("Have a good day!");
+}
+
+void Task()
+{
+    int[] arr = FillArray(ReadInt("an array length"));
+    PrintArray(arr);
+    System.Console.WriteLine(CountEven(arr));
+}
+
+int ReadInt(string argument)
+{
+    int number;
+    System.Console.WriteLine($"Input {argument}: ");
+    while(!int.TryParse(Console.ReadLine(), out number) || number <= 0)
+    {
+        System.Console.WriteLine("It's not that!");
+    }
+    return number;
+}
+
+int[] FillArray(int arrayLength)
+{
+    int[] array = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("Array: [ ");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        System.Console.Write(array[i] + "; ");
+    }
+    System.Console.WriteLine($"{array[array.Length-1]} ]");
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ hw_5/task_38/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Main();
+
+void Main()
+{
+    bool work = true;
+    while (work)
+    {
+        System.Console.WriteLine("Hello! Run the program? y/n");
+        string a = Console.ReadLine();
+        switch (a)
+        {
+            case "y": Task(); break;
+            case "n": work = false; break;
+            default: break;
+        }
+    }
+    System.Console.WriteLine("Have a good time!");
+}
+
+void Task()
+{
+    double[] arr = FillArray(ReadInt("an array length"));
+    PrintArray(arr);
+    System.Console.WriteLine($"Difference beetween maximum and minimum array elements is {DiffMaxMinOfArray(arr)}.");
+}
+
+int ReadInt(string argument)
+{
+    int number;
+    System.Console.WriteLine($"Input {argument}: ");
+    while(!int.TryParse(Console.ReadLine(), out number) || number <= 0)
+    {
+        System.Console.WriteLine("It's not that!");
+    }
+    return number;
+}
+
+double[] FillArray(int length)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() *(100 - -100) + -100, 2);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.Write("Array: [ ");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        System.Console.Write(array[i] + "; ");
+    }
+    System.Console.WriteLine(array[array.Length - 1] + " ].");
+}
+
+double DiffMaxMinOfArray(double[] array)
+{
+    if(array.Length == 1)
+    {
+        return array[0];
+    }
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if(array[i] > max)
+        {
+            max = array [i];
+        }
+        else if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return Math.Round(max - min, 2);
+}
